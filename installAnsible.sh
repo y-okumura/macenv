bbf77f813d4e72d3becb278c59a5cbb4ad1b904e
奥村悠 <uokumura@okumurayuunoMacBook-ea.local>
2017-11-04 00:21:13 +0900

    標準Pythonにpipを導入するのは困難なのでbrewを入れてbrewでAnsibleを入れるように変更

--- installAnsible.sh
+++
@@ -1,6 +0,0 @@
-xcode-select --install 
-curl https://bootstrap.pypa.io/ez_setup.py -o - | sudo python
-curl https://bootstrap.pypa.io/get-pip.py -O | sudo python
-sudo pip install ansible
-
-ansible-playbook dev-playbook.yml
