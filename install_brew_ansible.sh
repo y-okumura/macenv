6b33ec18dbbb3b7d46f0a0d35f9e0e4b33c1ecb0
奥村悠 <uokumura@okumurayuunoMacBook-ea.local>
2017-11-04 00:48:42 +0900

    brew入ってても怒られないように修正

--- install_brew_ansible.sh
+++ install_brew_ansible.sh
@@ -1,6 +1,10 @@
 xcode-select --install
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! type brew 2>/dev/null 1>/dev/null
+then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 brew install ansible
 
 ansible-playbook dev-playbook.yml
