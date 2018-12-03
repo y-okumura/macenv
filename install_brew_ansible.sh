395742ea07ed36bf25166293c222ad1ccfb9a0fa
Yu Okumura <y-okumura@nichiwa-system.co.jp>
2018-12-03 17:24:24 +0900

    Update install_brew_ansible.sh
    dotコマンドのある/usr/local/binにパスを通す。
    ほんとはこれもっと上のところでやることやけど...

--- install_brew_ansible.sh
+++ install_brew_ansible.sh
@@ -3,6 +3,7 @@ xcode-select --install
 if ! type brew 2>/dev/null 1>/dev/null
 then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  sudo sh -c "echo '/usr/local/bin' > /etc/paths.d/brewbin"
 fi
 
 brew install ansible
